46695ada6bc79afcdfe85995b0ca1d2392190311
Jay Bhuva <jaybhuva57@gmail.com>
2024-02-08 15:16:16 +0530

    1st done

---
+++ src/main/java/org/example/job_portal/service/EmployeeService.java
@@ -0,0 +1,33 @@
+package org.example.job_portal.service;
+
+import org.example.job_portal.model.Employee;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+public class EmployeeService implements IEmployeeService{
+    @Override
+    public Employee addEmployee(Employee employee) {
+        return null;
+    }
+
+    @Override
+    public List<Employee> getEmployee() {
+        return null;
+    }
+
+    @Override
+    public Employee updateEmployee(Employee employee) {
+        return null;
+    }
+
+    @Override
+    public void deleteEmployee(long id) {
+
+    }
+
+    @Override
+    public Employee getEmployeeById(long id) {
+        return null;
+    }
+}
